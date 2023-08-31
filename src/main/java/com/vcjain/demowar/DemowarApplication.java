15eb6c9abbb4b581e8d90d5190339516f1eb8cd3
Vikash Jain <vikash.jain@galaxyweblinks.co.in>
2023-08-31 08:45:45 +0530

    Initial commit

---
+++ src/main/java/com/vcjain/demowar/DemowarApplication.java
@@ -0,0 +1,13 @@
+package com.vcjain.demowar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcjain/demowar/DemowarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcjain.demowar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
